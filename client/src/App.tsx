ef983f519e6072ccd753b73cb32dcdb9195dbf6d
rabobahago <rabobahago@gmail.com>
2021-11-06 22:22:22 +0100

    initial project files added

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import ChatsListScreen from "./components/ChatsListScreen";
+
+const App: React.FC = () => (
+  //typescript
+  <div>
+    <ChatsListScreen />
+  </div>
+);
+export default App;
