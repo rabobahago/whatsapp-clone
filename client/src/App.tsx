6146ab761e426d66cf199beaf752141551aec564
rabobahago <rabobahago@gmail.com>
2021-11-06 23:19:26 +0100

    material UI and styled components css added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import ChatsListScreen from "./components/ChatsListScreen";
 
 const App: React.FC = () => (
-  //typescript
   <div>
     <ChatsListScreen />
   </div>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./index.css";
 import App from "./App";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#2c6157" },
+    secondary: { main: "#6fd056" }
+  }
+});
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />{" "}
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
