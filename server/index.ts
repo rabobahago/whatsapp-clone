35066c1a69e7e254d05d0672fdc84d7d6fac5672
rabobahago <rabobahago@gmail.com>
2021-11-07 14:27:42 +0100

    graphql,apollo server, queries, and resolvers added

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import { chats } from "./db";
+import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
+import schema from "./schema";
 
 const app = express();
-app.get("/_ping", (req, res) => {
-  res.send("pong");
-});
-app.get("/chats", (req, res) => {
-  res.json(chats);
+app.use(cors());
+app.use(express.json());
+
+const server = new ApolloServer({ schema });
+
+server.applyMiddleware({
+  app,
+  path: "/graphql"
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
